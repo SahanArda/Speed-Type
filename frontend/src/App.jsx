1157a32053ab8a8c4c0aac610dd8c144fe392e4b
Arda Sahan <ardambp@outlook.com>
2024-09-16 15:39:11 +0100

    created the frontend files and set up routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
